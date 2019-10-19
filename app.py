c1a8c61b9bc99331a365e7a0034c4d636d6a20d1
barry <barrypearseburke@gmail.com>
2019-10-19 20:53:02 +0100

    fake json data being returned added 2 sec sleep for demoing refresh in react

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+
+def fake_query(td_id):
+    pass
+
+
+@app.route('/votes')
+def hello_world():
+    td_id = request.args.get('td_id')
+    fake_query(td_id)
+    import time
+    time.sleep(2)
+    return jsonify({
+        "TD_Name": "Gerry Adams",
+        "Ta": 100,
+        "Nil": 50,
+        "Staonaim": 20
+    })
+
+
+if __name__ == '__main__':
+    app.run()
